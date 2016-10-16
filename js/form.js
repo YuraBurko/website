fad772b095473b684b1290fedd476087f281885d
Yura_Burko <yura_burko@mail.ru>
2016-10-16 19:09:58 +0000

    initial

--- js/form.js
+++ js/form.js
@@ -6,8 +6,8 @@ $(function () {
             method: "POST",
             data: {
                 message: $('#name').val(),
-                mail: $('#email').val()
-                text: $('#text').val()
+                mail: $('#email').val(),
+                text: $('#text').val(),
             },
             dataType: "json"
         }).done(function() {
